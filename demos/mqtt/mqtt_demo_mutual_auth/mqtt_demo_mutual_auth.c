a4e5c125ff5b81eec2a6a2f6ff07accd43dd3ae9
socar-rane <rane@socar.kr>
2021-04-19 12:21:46 +0900

    Update mqtt_demo_mutual_auth.c
    add publish options

--- demos/mqtt/mqtt_demo_mutual_auth/mqtt_demo_mutual_auth.c
+++ demos/mqtt/mqtt_demo_mutual_auth/mqtt_demo_mutual_auth.c
@@ -2132,7 +2132,7 @@ int main( int argc, char ** argv )
 
         int option_index = 0;
 
-        c = getopt_long(argc, argv, "c:C:fhm:pst:", long_options, &option_index);
+        c = getopt_long(argc, argv, "c:C:fhm:p:st:", long_options, &option_index);
 
         if(c == -1)
             break;
